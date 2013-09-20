8b7725bed26d67d58289be088e35cc9e542d11d6
Shaun Jackman <sjackman@gmail.com>
2013-09-19 17:16:38 -0700

    masruca: Fix compiler errors on OS X

--- masurca.rb
+++ masurca.rb
@@ -7,13 +7,58 @@ class Masurca < Formula
 
   keg_only 'MaSuRCA conflicts with jellyfish.'
 
+  depends_on 'pkg-config' => :build
+
+  fails_with :clang do
+    build 425
+    cause 'Jellyfish fails to build with clang.'
+  end
+
+  fails_with :llvm do
+    build 2336
+    cause 'Jellyfish fails to build with llvm-gcc.'
+  end
+  
+  def patches
+    # Fix the Jellyfish compiler error.
+    DATA
+  end
+
   def install
     ENV.j1
     ENV['DEST'] = prefix
-    system './install.sh'
+    system 'sh -e ./install.sh'
   end
 
   test do
     system "#{bin}/runSRCA.pl"
   end
 end
+
+__END__
+diff --git a/jellyfish-1.1.10/jellyfish/err.hpp b/jellyfish-1.1.10/jellyfish/err.hpp
+index 14fc74b..93de4a0 100644
+--- a/jellyfish-1.1.10/jellyfish/err.hpp
++++ b/jellyfish-1.1.10/jellyfish/err.hpp
+@@ -41,7 +41,7 @@ namespace err {
+     char  err_str[1024];
+     char* str;
+ 
+-#if (_POSIX_C_SOURCE >= 200112L || _XOPEN_SOURCE >= 600) && ! _GNU_SOURCE
++#if 1 // (_POSIX_C_SOURCE >= 200112L || _XOPEN_SOURCE >= 600) && ! _GNU_SOURCE
+     int err = strerror_r(e, err_str, sizeof(err_str));
+     if(err)
+       strcpy(err_str, "error");
+diff --git a/jellyfish-2.0.0beta6.1/include/jellyfish/err.hpp b/jellyfish-2.0.0beta6.1/include/jellyfish/err.hpp
+index 3ebc324..6c5b846 100644
+--- a/jellyfish-2.0.0beta6.1/include/jellyfish/err.hpp
++++ b/jellyfish-2.0.0beta6.1/include/jellyfish/err.hpp
+@@ -42,7 +42,7 @@ public:
+     char  err_str[1024];
+     char* str;
+ 
+-#if (_POSIX_C_SOURCE >= 200112L || _XOPEN_SOURCE >= 600) && ! _GNU_SOURCE
++#if 1 // (_POSIX_C_SOURCE >= 200112L || _XOPEN_SOURCE >= 600) && ! _GNU_SOURCE
+     int err = strerror_r(e, err_str, sizeof(err_str));
+     if(err)
+       strcpy(err_str, "error");
