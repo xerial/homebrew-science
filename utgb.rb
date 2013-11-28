5efda093e3695b1a8f7591f67e7a3e13a594e189
Taro L. Saito <leo@xerial.org>
2013-11-28 10:15:19 +0900

    Use libexec folder to place the utgb package, then create a symlink to bin/utgb

--- utgb.rb
+++ utgb.rb
@@ -5,13 +5,10 @@ class Utgb < Formula
   url 'http://maven.utgenome.org/repository/artifact/org/utgenome/utgb-shell/1.5.9/utgb-shell-1.5.9-bin.tar.gz'
   sha1 'fe4aaccfca40df85c495073385f1a5ff904ac893'
   version '1.5.9'
-
+  
   def install
-     bin.install Dir['bin/utgb']
-     bin.install Dir['bin/*.conf']
-     prefix.install Dir['boot']
-     prefix.install Dir['lib']
-     bin.install_symlink Dir['bin/utgb']
+     libexec.install Dir['*']
+     bin.install_symlink "#{libexec}/bin/utgb"
   end
 
   def test
